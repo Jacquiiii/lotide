b47781de95a8e726f0ae3ac840333677bba55e8a
Jacquiiii <jacqui.koroll@telus.com>
2022-11-14 17:07:01 -0800

    improved code using template literals in console log statements

--- assertEqual.js
+++ assertEqual.js
@@ -1,10 +1,10 @@
 // Function that compares two values and returns a passed or failure message if the values are equal or not
 
 const assertEqual = (actual, expected) => {
-  actual === expected ? console.log("✅✅✅✅ Assertion Passed: " + actual + " === " + expected) : console.log("🛑🛑🛑🛑 Assertion Failed: " + actual + " !== " + expected);
+  actual === expected ? console.log(`✅✅✅✅ Assertion Passed: ${actual} === ${expected}`) : console.log(`🛑🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
 };
 
-// TEST CODE
+// Test code
 assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
 assertEqual("Lighthouse Labs", "Lighthouse Labs");
