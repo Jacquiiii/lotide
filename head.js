9bad9b6f69d57db966d9d7079c68a5f774a323be
Jacquiiii <jacqui.koroll@telus.com>
2022-11-15 07:38:12 -0800

    updated version of head code

--- head.js
+++ head.js
@@ -4,19 +4,27 @@ const assertEqual = (actual, expected) => {
 };
 
 // Function that returns the first element in an array if there is at least one and returns undefined if not
+
 const head = (arr) => {
 
   if (arr.length >= 1) {
     let firstElement = arr.shift(1);
     return firstElement;
-  } else {
+  } else if (!Array.isArray(arr) || arr.length === 0) {
     return undefined;
   }
 
 };
 
-// Test code
+// Test code for head function
+console.log(head([5, 2, 1]));
+console.log(head([5]));
+console.log(head(2));
+console.log(head([]));
+
+// Test code for assert equal
 assertEqual(head([5,6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
 assertEqual(head([6]), 5);
 assertEqual(head([]), undefined);
+assertEqual(head(2), undefined);
