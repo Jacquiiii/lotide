742d6df73379c227cb9f0205dbe5f0aa3be81b48
Jacquiiii <jacqui.koroll@telus.com>
2022-11-16 12:24:35 -0800

    finished product of middle.js which is function that returns the middle of an array

--- middle.js
+++ middle.js
@@ -15,19 +15,7 @@ const assertArraysEqual = (arr1, arr2) => {
 };
 
 
-
-// Implement middle which will take in an array and return the middle-most element(s) of the given array.
-//The middle function should return an array with only the middle element(s) of the provided array. This means that the length of the returned elements could vary.
-//For arrays with one or two elements, there is no middle. Return an empty array.
-//For arrays with odd number of elements, an array containing a single middle element should be returned.
-//For arrays with an even number of elements, an array containing the two elements in the middle should be returned
-
-// Plan:
-//
-//
-//
-//
-
+//function that returns the middle of an array
 const middle = array => {
   
   if (array.length === 0 || array.length === 1 || array.length === 2) {
