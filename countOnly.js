046d4492e8df5a05c3d3d326a69e19f9bf32f321
Jacquiiii <jacqui.koroll@telus.com>
2022-11-16 12:51:49 -0800

    added Function that should take in a collection of items and return counts for a specific subset of those items.

---
+++ countOnly.js
@@ -0,0 +1,40 @@
+// Function that compares two values and returns a passed or failure message if the values are equal or not
+const assertEqual = (actual, expected) => {
+  actual === expected ? console.log(`✅✅✅✅ Assertion Passed: ${actual} === ${expected}`) : console.log(`🛑🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+};
+
+
+const countOnly = function(allItems, itemsToCount) {
+  let results = {};
+
+  for (let item of allItems) {
+    if (itemsToCount[item]) { 
+      if (results[item]) {
+        results[item] += 1;
+      } else {
+        results[item] = 1;
+      }
+    }
+  }
+
+  return results;
+}
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
