26ca7e4d6c0b3362f7a29b9f052534b202eb434f
Jacquiiii <jacqui.koroll@telus.com>
2022-11-17 07:24:40 -0800

    refactored eqObjects.js

--- sandbox.js
+++ sandbox.js
@@ -13,27 +13,24 @@ const eqArrays = (arr1, arr2) => {
 };
 
 
-// Project function: Function that compares two objects and returns true if they match or false if they don't
-const eqObjects = (object1, object2) => {
-  let keys1 = Object.keys(object1);
-  let keys2 = Object.keys(object2);
 
-  if (keys1.length !== keys2.length) {
-    return false;
+// Function compares two objects to check if they are exactly the same
+const eqObjects = function(object1, object2) {
+  if (Object.keys(object1).length !== Object.keys(object2).length) { // Confirms if objects both contain same amount of keys
+    return false; // returns false if object key length !same
   }
 
-  for (let key of keys1) {
-
-    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
-      if (!eqArrays(object1[key], object2[key])) {
-        return false;
+  for (const key in object1) {
+    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) { //checks if true that both keys contain arrays
+      if (!eqArrays(object1[key], object2[key])) { //uses helper function eqArrays to see if key arrays are the same
+        return false; // return false if key arrays !same
       }
-
-    } else if (object1[key] !== object2[key]) {
-      return false;
+    } else if (object1[key] !== object2[key]) { //compares object keys primitives to see if the same
+      return false; // return false if key primitives !same
     }
   }
-  return true;
+  
+  return true; // return true if no falsey statements above, objects are identical
 };
 
 
@@ -57,11 +54,3 @@ assertEqual(eqObjects(ab, abc), false);
 assertEqual(eqObjects(ab, jb), false);
 assertEqual(eqObjects(cd, dc), true);
 assertEqual(eqObjects(cd, cd2), false);
-
-// Console logs to go at beginning of for loop to understand what each loop is producing
-// console.log(`Key one: ${key}`);
-// console.log(`Value of key 1: ${object1[key]}`);
-// console.log(`Value of key 2: ${object2[key]}`);
-
-
-
