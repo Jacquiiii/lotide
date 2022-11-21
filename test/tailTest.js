97a5f969dc293db8407580b18fe8b3ba0a82d654
Jacquiiii <jacqui.koroll@telus.com>
2022-11-21 14:04:02 -0800

    removed json file from git ignore

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,9 +1,26 @@
-const assertEqual = require('../assertEqual');
+// const assertEqual = require('../assertEqual');
+// const tail = require('../tail');
+
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// tail(words); 
+// assertEqual(words.length, 3); 
+
+// let result = tail(["Hello", "Lighthouse", "Labs"]);
+// assertEqual(result.toString(), ["Lighthouse", "Labs"].toString());
+
+
+
+const assert = require('chai').assert;
 const tail = require('../tail');
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+describe("#tail", () => {
 
-let result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.toString(), ["Lighthouse", "Labs"].toString());
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.deepEqual(tail([1, 2, 3, 4, 5]), [2, 3, 4, 5]);
+  });
+
+  it("returns undefined for 2", () => {
+    assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]); 
+  });
+
+});
