9df759603c6a08b89c0de70553b4a1506e1b9d42
Jacquiiii <jacqui.koroll@telus.com>
2022-11-21 13:11:56 -0800

    updated headTest.js with chai assertions

--- test/headTest.js
+++ test/headTest.js
@@ -1,7 +1,30 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const head = require('../head');
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), undefined);
-assertEqual(head(2), undefined);
+describe("#head", () => {
+
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
+
+  it("returns 5 for [5,6,7]", () => {
+    assert.strictEqual(head([5,6,7]), 5); 
+  });
+
+  it('returns "Hello" for ["Hello", "Lighthouse", "Labs"]', () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"); 
+  });
+
+  it("returns undefined for []", () => {
+    assert.strictEqual(head([]), undefined); 
+  });
+
+  it("returns undefined for 2", () => {
+    assert.strictEqual(head(2), undefined); 
+  });
+
+});
