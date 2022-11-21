c82fb389a32f32ad3cd3cb2953189431259f4d2b
Jacquiiii <jacqui.koroll@telus.com>
2022-11-21 14:10:08 -0800

    updated tailTest.js with chai assertions

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,15 +1,3 @@
-// const assertEqual = require('../assertEqual');
-// const tail = require('../tail');
-
-// const words = ["Yo Yo", "Lighthouse", "Labs"];
-// tail(words); 
-// assertEqual(words.length, 3); 
-
-// let result = tail(["Hello", "Lighthouse", "Labs"]);
-// assertEqual(result.toString(), ["Lighthouse", "Labs"].toString());
-
-
-
 const assert = require('chai').assert;
 const tail = require('../tail');
 
