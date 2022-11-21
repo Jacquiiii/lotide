ce5d334e6fa14a4e8c77d89d4f588420ad84088c
Jacquiiii <jacqui.koroll@telus.com>
2022-11-21 14:51:08 -0800

    adding index.js file

---
+++ index.js
@@ -0,0 +1,32 @@
+const head = require('./head');
+const tail = require('./tail');
+const middle = require('./middle');
+const countLetters = require('./countLetters');
+const countOnly = require('./countOnly');
+const eqArrays = require('./eqArrays');
+const eqObjects = require('./eqObjects');
+const findKey = require('./findKey');
+const findKeyByValue = require('./findKeyByValue');
+const letterPositions = require('./letterPositions');
+const map = require('./map');
+const minimumValues = require('./minimum_values');
+const takeUntil = require('./takeUntil');
+const without = require('./without');
+
+
+module.exports = {
+  head: head,
+  tail: tail,
+  middle: middle,
+  countLetters: countLetters,
+  countOnly: countOnly,
+  eqArrays: eqArrays,
+  eqObjects: eqObjects,
+  findKey: findKey,
+  findKeyByValue: findKeyByValue,
+  letterPositions: letterPositions,
+  map: map,
+  minimumValues: minimumValues,
+  takeUntil: takeUntil,
+  without: without
+};
